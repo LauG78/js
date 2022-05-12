5337b2f5a859ec494960668e563ae5f2ff102c60
LauG78 <byke.g20@gmail.com>
2022-05-12 12:07:24 +0200

    ex 08 dom

--- 06-dom/08-match-password-one/script.js
+++ 06-dom/08-match-password-one/script.js
@@ -10,7 +10,16 @@
 // You will have time to focus on it later.
 
 (function() {
-
+    document.getElementById("run").addEventListener("click",()=>{
+        let passoneval = document.getElementById("pass-one").value;
+        let passtwoval = document.getElementById("pass-two").value;
+        let passone = document.getElementById("pass-one");
+        let passtwo = document.getElementById("pass-two");
+        if(passoneval !== passtwoval){
+            passone.style.borderColor = "red";
+            passtwo.style.borderColor = "red";
+        }
+    })
     // your code here
 
 })();
