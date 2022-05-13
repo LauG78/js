616a3f21feaacc4cb101b99d52908f744e549119
LauG78 <byke.g20@gmail.com>
2022-05-13 16:21:34 +0200

    02-fetch

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -10,5 +10,21 @@
 // You will have time to focus on it later.
 
 (() => {
+    document.getElementById("run").addEventListener("click", ()=>{
+        let hero = document.getElementById("hero-id").value;
+        let tpl_hero = document.getElementById("tpl-hero");
+        let target = document.getElementById("target");
+        let clone = tpl_hero.content.cloneNode(true);
+        fetch(`http://localhost:3000/heroes/${hero}`).then(res=>res.json()).then(data=>{
+            let name = clone.querySelector(".name");
+            let alter_ego = clone.querySelector(".alter-ego");
+            let powers = clone.querySelector(".powers");
+            name.innerText = data.name;
+            alter_ego.innerText = data.alterEgo;
+            powers.innerText = data.abilities;
+            target.appendChild(clone);
+        }).catch(err=>alert("That one's dead!"))
+        })
+    
     // your code here
 })();
